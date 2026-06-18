06f51d3523123515165ecb42262dc2189b9894bd
Moch. Agil Sugiarto <mochamadagils@gmail.com>
2026-06-18 19:27:24 +0700

    new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,11 +19,22 @@ export const projects = [
     id: 2,
     name: 'OutVentura',
     description:
-      'Aplikasi web capstone project dengan integrasi API, fitur analisis, dan tampilan dashboard interaktif.',
+      'Aplikasi web tugas kuliah dengan integrasi API, fitur analisis, dan tampilan dashboard interaktif.',
     techStack: ['React.js', 'Express.js', 'PostgreSQL / Supabase', 'MidTrans'],
     githubUrl: 'https://github.com/MAgSAsli/OutVentura',
     demoUrl: 'https://out-ventura-vpiw.vercel.app/',
     image: '/projects/Outventura.jpg',
     highlight: 'Tugas Kuliah',
   },
+  {
+  id: 3,
+  name: 'VentureCraft',
+  description:
+    'Aplikasi web capstone project Dicoding untuk membantu menganalisis kelayakan ide bisnis berdasarkan modal, biaya operasional, harga jual, target pembeli, dan estimasi keuntungan. Aplikasi ini menampilkan hasil analisis seperti skor kelayakan, risiko, BEP, ROI, payback period, serta dashboard interaktif.',
+  techStack: ['React.js', 'Express.js', 'Supabase', 'Tailwind CSS'],
+  githubUrl: '',
+  demoUrl: 'https://venture-craft-96kf.vercel.app/',
+  image: '/projects/VentureCraft.jpg',
+  highlight: 'Capstone Project',
+}
 ];
